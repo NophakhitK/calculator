0393d46c7378e1bbbf952e7b69f253e2ee4035fe
Nophakhit Khositchaiphithak <nophakhit.k@gmail.com>
2023-02-23 15:03:50 +0700

    Added operator function

--- js/script.js
+++ js/script.js
@@ -1,24 +1,42 @@
+let output = 0;
+let total = 0;
+
+const operate = function (a, operator, b) {
+    if (operator === "+") {
+        add(a, b);
+        total = output;
+        return total;
+    }
+    if (operator === "-") {
+        subtract(a, b);
+        total = output;
+        return total;
+    }
+    if (operator === "*") {
+        multiply(a, b);
+        total = output
+        return total;
+    }
+    if (operator === "/") {
+        divide(a, b);
+        total = output;
+        return total;
+    }
+};
+
 const add = function (a, b) {
-    let total = 0;
-    total = a + b;
-    return total;
+    return output = a + b;
 };
 
 const subtract = function (a, b) {
-    let total = 0;
-    total = a - b;
-    return total;
+    return output = a - b;
 }
 
 const multiply = function (a, b) {
-    let total = 1;
-    total = a * b;
-    return total;
+    return output = a * b;
 }
 
 const divide = function (a, b) {
-    let total = 1;
-    total = a / b;
-    return total;
+    return output = a / b;
 }
 

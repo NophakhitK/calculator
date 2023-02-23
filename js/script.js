10f880244437f1668549553b94413663717f4555
Nophakhit Khositchaiphithak <nophakhit.k@gmail.com>
2023-02-23 11:56:48 +0700

    Added basic math functions

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const add = function (a, b) {
+    let total = 0;
+    total = a + b;
+    return total;
+};
+
+const subtract = function (a, b) {
+    let total = 0;
+    total = a - b;
+    return total;
+}
+
+const multiply = function (a, b) {
+    let total = 1;
+    total = a * b;
+    return total;
+}
+
+const divide = function (a, b) {
+    let total = 1;
+    total = a / b;
+    return total;
+}
+

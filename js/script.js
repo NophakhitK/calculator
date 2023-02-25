86dd8b52f40867310775b4302494571dbea50125
Nophakhit Khositchaiphithak <nophakhit.k@gmail.com>
2023-02-25 15:48:44 +0700

    Added slice string function to seperate display value to three parts

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
 let output = 0;
 let total = 0;
-let storedValue = "";
+let displayValue = "";
+let storedValue = displayValue;
+let firstPart = 0;
+let secondPart = 0;
+let operatorChosen = "";
 
 const upperScreenValue = document.querySelector('.upperScreenDisplay');
 const displayValueSelected = document.querySelector('.displayValue')
 const allInputs = document.querySelectorAll('.rows > button');
-const allOperators = document.querySelectorAll('.operators');
+// const allOperators = document.querySelectorAll('.operators');
+const plusOperator = document.getElementById('plusOperator');
+const subtractOperator = document.getElementById('subtractOperator');
+const multiplyOperator = document.getElementById('multiplyOperator');
+const divideOperator = document.getElementById('divideOperator');
+const equalOperator = document.getElementById('equalOperator');
+const functionOperators = [plusOperator, subtractOperator, multiplyOperator, divideOperator];
+
+
 
 const valueSelected = function () {
     for (const input of allInputs) {
         input.addEventListener('click', function (e) {
-            checkOperator();
-            storedValue += e.target.value;
-            displayValueSelected.textContent = storedValue;
+            displayValue += e.target.value;
+            displayValueSelected.textContent = displayValue;
+            console.log(displayValue);
         });
     }
 }
-
 
 
 
@@ -31,12 +42,13 @@ const operate = function (a, operator, b) {
         total = output;
         return total;
     }
-    if (operator === "*") {
+    if (operator === "x") {
         multiply(a, b);
         total = output
         return total;
     }
     if (operator === "÷") {
+        ห
         divide(a, b);
         total = output;
         return total;
@@ -61,13 +73,49 @@ const divide = function (a, b) {
 
 // const checkOperator = function () {
 //     for (const operator of allOperators) {
-//         if (storedValue.includes(operator.value)) {
+//         if (storedValue.contains(operator)) {
 //             return true;
 //         } else {
 //             return false;
 //         }
 //     }
 // }
+
+
+
+const findIndexOperator = function () {
+    const plusOperatorIndex = displayValue.indexOf(plusOperator);
+    const subtractOperatorIndex = displayValue.indexOf(subtractOperator);
+    const multiplyOperatorIndex = displayValue.indexOf(multiplyOperator);
+    const divideOperatorIndex = displayValue.indexOf(divideOperator);
+
+}
+
+
+
+
+const sliceString = function () {
+    plusOperatorIndex = findPlusOperatorIndex();
+    equalOperatorIndex = findEqualOperatorIndex();
+    firstPart = displayValue.slice(0, plusOperatorIndex);
+    console.log(firstPart);
+    operatorChosen = displayValue.slice(plusOperatorIndex, plusOperatorIndex + 1);
+    console.log(operatorChosen);
+    secondPart = displayValue.slice(plusOperatorIndex + 1, equalOperatorIndex);
+    console.log(secondPart);
+}
+
+const findPlusOperatorIndex = function () {
+    const plusOperatorIndex = displayValue.indexOf(plusOperator.value);
+    console.log(plusOperatorIndex)
+    return plusOperatorIndex;
+}
+const findEqualOperatorIndex = function () {
+    const equalOperatorIndex = displayValue.indexOf(equalOperator.value);
+    console.log(equalOperatorIndex);
+    return equalOperatorIndex;
+}
+
 
 valueSelected();
 

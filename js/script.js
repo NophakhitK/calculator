1e59d29f739f6e937d7f73fcb3a1b67319896735
Nophakhit Khositchaiphithak <nophakhit.k@gmail.com>
2023-02-25 20:03:28 +0700

    Rewrote slice string function and find index function

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
 let output = 0;
 let total = 0;
 let displayValue = "";
-let storedValue = displayValue;
+let storedValue = "";
 let firstPart = 0;
 let secondPart = 0;
 let operatorChosen = "";
+let index = 0;
 
 const upperScreenValue = document.querySelector('.upperScreenDisplay');
 const displayValueSelected = document.querySelector('.displayValue')
 const allInputs = document.querySelectorAll('.rows > button');
-// const allOperators = document.querySelectorAll('.operators');
+
 const plusOperator = document.getElementById('plusOperator');
 const subtractOperator = document.getElementById('subtractOperator');
 const multiplyOperator = document.getElementById('multiplyOperator');
 const divideOperator = document.getElementById('divideOperator');
 const equalOperator = document.getElementById('equalOperator');
-const functionOperators = [plusOperator, subtractOperator, multiplyOperator, divideOperator];
-
-
+const allOperators = document.getElementsByClassName('operators')
 
 const valueSelected = function () {
     for (const input of allInputs) {
@@ -31,33 +30,85 @@ const valueSelected = function () {
 
 
 
-const operate = function (a, operator, b) {
-    sliceString();
+
+
+for (const allOperator of allOperators) {
+    allOperator.addEventListener('click', () => {
+        if (allOperator.value === plusOperator.value) {
+            storedValue = displayValue;
+            storedValue += plusOperator.value;
+            index = findPlusOperatorIndex();
+            firstPart = storedValue.slice(0, index);
+            operatorChosen = allOperator.value;
+        } if (allOperator.value === subtractOperator.value) {
+            index = findSubtractOperatorIndex();
+            firstPart = displayValue.slice(0, index);
+            console.log(firstPart);
+            operatorChosen = allOperator.value;
+            console.log(operatorChosen);
+        } if (allOperator.value === multiplyOperator.value) {
+            index = findMultiplyOperatorIndex();
+            firstPart = displayValue.slice(0, index);
+            console.log(firstPart);
+            operatorChosen = allOperator.value;
+            console.log(operatorChosen);
+        } if (allOperator.value === divideOperator.value) {
+            index = findDivideOperatorIndex();
+            firstPart = displayValue.slice(0, index);
+            console.log(firstPart);
+            operatorChosen = allOperator.value;
+            console.log(operatorChosen);
+        }
+    })
+}
+
+
+const sliceStringSecondPart = function () {
+    storedValue = displayValue;
+    storedValue += equalOperator.value;
+    equalIndex = findEqualOperatorIndex();
+    secondPart = storedValue.slice(index, equalIndex);
+}
+
+equalOperator.addEventListener('click', () => {
     a = parseInt(firstPart);
+    console.log(a);
+    console.log(typeof a);
     operator = operatorChosen;
+    console.log(operator);
+    console.log(typeof operator);
+    sliceStringSecondPart();
     b = parseInt(secondPart);
-    if (operator === "+") {
-        add(a, b);
-        total = output;
-        return total;
-    }
-    if (operator === "-") {
-        subtract(a, b);
-        total = output;
-        return total;
-    }
-    if (operator === "x") {
-        multiply(a, b);
-        total = output
-        return total;
-    }
-    if (operator === "÷") {
-        ห
-        divide(a, b);
-        total = output;
-        return total;
-    }
-};
+    console.log(b);
+    console.log(typeof b)
+    operate(a, operator, b);
+    console.log(total);
+});
+
+const findPlusOperatorIndex = function () {
+    const plusOperatorIndex = storedValue.indexOf(plusOperator.value);
+    return plusOperatorIndex;
+}
+
+const findSubtractOperatorIndex = function () {
+    const subtractOperatorIndex = displayValue.indexOf(subtractOperator.value);
+    return subtractOperatorIndex;
+}
+
+const findMultiplyOperatorIndex = function () {
+    const multiplyOperatorIndex = displayValue.indexOf(multiplyOperator.value);
+    return multiplyOperatorIndex;
+}
+
+const findDivideOperatorIndex = function () {
+    const divideOperatorIndex = displayValue.indexOf(divideOperator.value);
+    return divideOperatorIndex;
+}
+
+const findEqualOperatorIndex = function () {
+    const equalOperatorIndex = storedValue.indexOf(equalOperator.value);
+    return equalOperatorIndex;
+}
 
 const add = function (a, b) {
     return output = a + b;
@@ -75,47 +126,30 @@ const divide = function (a, b) {
     return output = a / b;
 }
 
-// const checkOperator = function () {
-//     for (const operator of allOperators) {
-//         if (storedValue.contains(operator)) {
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     }
-// }
+const operate = function (a, operator, b) {
+    if (operator === "+") {
+        add(a, b);
+        total = output;
+        return total;
+    }
+    if (operator === "-") {
+        subtract(a, b);
+        total = output;
+        return total;
+    }
+    if (operator === "x") {
+        multiply(a, b);
+        total = output
+        return total;
+    }
+    if (operator === "÷") {
+        divide(a, b);
+        total = output;
+        return total;
+    }
+    return total;
+};
 
-
-
-// const findIndexOperator = function () {
-//     const plusOperatorIndex = displayValue.indexOf(plusOperator);
-//     const subtractOperatorIndex = displayValue.indexOf(subtractOperator);
-//     const multiplyOperatorIndex = displayValue.indexOf(multiplyOperator);
-//     const divideOperatorIndex = displayValue.indexOf(divideOperator);
-// }
-
-
-const sliceString = function () {
-    plusOperatorIndex = findPlusOperatorIndex();
-    equalOperatorIndex = findEqualOperatorIndex();
-    firstPart = displayValue.slice(0, plusOperatorIndex);
-    console.log(firstPart);
-    operatorChosen = displayValue.slice(plusOperatorIndex, plusOperatorIndex + 1);
-    console.log(operatorChosen);
-    secondPart = displayValue.slice(plusOperatorIndex + 1, equalOperatorIndex);
-    console.log(secondPart);
-}
-
-const findPlusOperatorIndex = function () {
-    const plusOperatorIndex = displayValue.indexOf(plusOperator.value);
-    console.log(plusOperatorIndex)
-    return plusOperatorIndex;
-}
-const findEqualOperatorIndex = function () {
-    const equalOperatorIndex = displayValue.indexOf(equalOperator.value);
-    console.log(equalOperatorIndex);
-    return equalOperatorIndex;
-}
 
 
 valueSelected();

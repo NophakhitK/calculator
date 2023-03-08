859dba3d2df3086379a7d2afd88e919efed74d42
Nophakhit Khositchaiphithak <nophakhit.k@gmail.com>
2023-03-08 17:04:31 +0700

    Refactored code and fixed bugs issue

--- js/script.js
+++ js/script.js
@@ -1,78 +1,144 @@
+// functions for calculator
 
-
-
-
-const add = function (a, b) {
-    return output = a + b;
+const add = function (firstValue, secondValue) {
+    return output = firstValue + secondValue;
 };
 
-const subtract = function (a, b) {
-    return output = a - b;
+const subtract = function (firstValue, secondValue) {
+    return output = firstValue - secondValue;
 }
 
-const multiply = function (a, b) {
-    return output = a * b;
+const multiply = function (firstValue, secondValue) {
+    return output = firstValue * secondValue;
 }
 
-const divide = function (a, b) {
-    return output = a / b;
+const divide = function (firstValue, secondValue) {
+    return output = firstValue / secondValue;
 }
 
-const operate = function (a, operator, b) {
-    if (operator === "+") {
-        add(a, b);
+const power = function (firstValue, secondValue) {
+    return output = firstValue ** secondValue;
+}
+
+const operate = function (firstValue, operator, secondValue) {
+    if (operator === "plus") {
+        add(firstValue, secondValue);
         total = output;
-        return total;
     }
-    if (operator === "-") {
-        subtract(a, b);
+    if (operator === "subtract") {
+        subtract(firstValue, secondValue);
         total = output;
-        return total;
     }
-    if (operator === "x") {
-        multiply(a, b);
+    if (operator === "multiply") {
+        multiply(firstValue, secondValue);
         total = output
-        return total;
     }
-    if (operator === "÷") {
-        divide(a, b);
+    if (operator === "divide") {
+        divide(firstValue, secondValue);
         total = output;
-        return total;
+    }
+    if (operator === "power") {
+        power(firstValue, secondValue);
+        total = output;
     }
     return total;
 };
 
+// Function for round decimal to 4 places
+function roundLongDecimals(total) {
+    if (total.toString().indexOf('.') !== -1) {
+        if (total.toString().split('.')[1].length > 4) {
+            return total.toFixed(4);
+        }
+    }
+    return total;
+}
 
+const calculator = document.querySelector('.calculator');
+let displayValue = document.querySelector('.displayValue');
+const buttonKeys = document.querySelectorAll('.rows > button');
+const allOperators = document.querySelectorAll('.operators');
 
+for (const buttonKey of buttonKeys) {
+    buttonKey.addEventListener('click', (e) => {
+        if (e.target.matches('button')) {
+            const key = e.target;
+            const action = key.dataset.action;
+            let displaySelected = displayValue.textContent;
+            const previousKeyType = calculator.dataset.previousKeyType;
+            const keyContent = key.textContent;
+            if (!action) {
+                if (displaySelected === '0' || previousKeyType === 'operator' || previousKeyType === 'calculate') {
+                    displayValue.textContent = keyContent;
+                } else {
+                    displayValue.textContent = displaySelected + keyContent
+                }
+                calculator.dataset.previousKeyType = 'number';
+            }
 
+            if (action === 'plus' ||
+                action === 'subtract' ||
+                action === 'multiply' ||
+                action === 'divide' ||
+                action === 'power'
+            ) {
+                const firstValue = parseFloat(calculator.dataset.firstValue);
+                const operator = calculator.dataset.operator;
+                const secondValue = parseFloat(displaySelected);
+                if (firstValue && operator && previousKeyType !== 'operator' && previousKeyType !== 'calculate') {
+                    const calculatedValue = operate(firstValue, operator, secondValue);
+                    displayValue.textContent = calculatedValue;
 
+                    // Update calculate value as first value
+                    calculator.dataset.firstValue = calculatedValue;
+                } else {
+                    // If there are no calculation, set displayed number as first value
+                    calculator.dataset.firstValue = displaySelected
+                }
+                calculator.dataset.previousKeyType = 'operator';
+                calculator.dataset.operator = action
+            }
 
+            if (action === 'decimal') {
+                if (!displaySelected.includes('.')) {
+                    displayValue.textContent = displaySelected + '.';
+                } else if (previousKeyType === 'operator' || previousKeyType === 'calculate') {
+                    displayValue.textContent = '0.'
+                }
+                calculator.dataset.previousKeyType = 'decimal'
+            }
+            if (action === 'clearAll') {
+                calculator.dataset.firstValue = '';
+                calculator.dataset.modValue = '';
+                calculator.dataset.operator = '';
+                calculator.dataset.previousKeyType = '';
+                displayValue.textContent = 0;
+                calculator.dataset.previousKeyType = 'clearAll';
+            }
+            if (action === 'clearDisplay') {
+                displayValue.textContent = 0;
+                calculator.dataset.previousKeyType = 'clearDisplay';
+            }
+            if (action === 'calculate') {
+                let firstValue = parseFloat(calculator.dataset.firstValue);
+                const operator = calculator.dataset.operator;
+                const secondValue = parseFloat(displaySelected);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                if (firstValue) {
+                    if (previousKeyType === 'calculate') {
+                        firstValue = displaySelected;
+                        secondValue = calculator.dataset.modValue;
+                    }
+                    total = operate(firstValue, operator, secondValue);
+                    displayValue.textContent = roundLongDecimals(total);
+                }
+                // Set modifier value attribute
+                calculator.dataset.modValue = secondValue;
+                calculator.dataset.previousKeyType = 'calculate'
+            }
+        }
+    });
+}
 
 
 // let output = 0;
@@ -96,7 +162,6 @@ const operate = function (a, operator, b) {
 // const equalOperator = document.getElementById('equalOperator');
 // const allOperators = document.getElementsByClassName('operators')
 // const clearButton = document.getElementById('clearAll');
-
 
 // for (const input of allInputs) {
 //     input.addEventListener('click', function (e) {
@@ -146,7 +211,6 @@ const operate = function (a, operator, b) {
 //         }
 //     })
 // }
-
 
 // const sliceStringSecondPart = function () {
 //     storedValue = displayValue;
@@ -209,4 +273,79 @@ const operate = function (a, operator, b) {
 // }
 
 
+// _____________ TEST 2 ________________
 
+// const calculator = document.querySelector('.calculator');
+// const buttonKeys = calculator.querySelectorAll('.rows > button');
+// const upperScreenDisplay = document.querySelector('.displayValue');
+
+// let a = 0;
+// let b = 0;
+// let total = 0;
+
+// for (const buttonKey of buttonKeys) {
+//     buttonKey.addEventListener('click', e => {
+//         if (e.target.matches('button')) {
+//             const key = e.target;
+//             const action = key.dataset.action;
+//             const keyContent = key.textContent;
+//             const previousKeyType = calculator.dataset.previousKeyType;
+//             const displayedSelected = upperScreenDisplay.textContent;
+//             if (!action) {
+//                 if (displayedSelected === '0' || previousKeyType === 'operator') {
+//                     upperScreenDisplay.textContent = keyContent;
+//                 } else {
+//                     upperScreenDisplay.textContent = displayedSelected + keyContent;
+//                 }
+//                 calculator.dataset.previousKeyType = 'number';
+//             }
+//             if (
+//                 action === 'plus' ||
+//                 action === 'subtract' ||
+//                 action === 'multiply' ||
+//                 action === 'divide' ||
+//                 action === 'power'
+//             ) {
+//                 calculator.dataset.firstValue = displayedSelected
+//                 calculator.dataset.operator = action
+//                 calculator.dataset.previousKeyType = 'operator';
+
+//             }
+//             if (action === 'decimal') {
+//                 if (!displayedSelected.includes('.')) {
+//                     upperScreenDisplay.textContent = displayedSelected + '.'
+//                 }
+//                 if (previousKeyType === 'operator') {
+//                     upperScreenDisplay.textContent = '0.'
+//                 }
+//                 calculator.dataset.previousKeyType = 'decimal';
+//             }
+//             if (action === 'clear') {
+//                 console.log('clear');
+//             }
+//             if (action === 'clearAll') {
+//                 console.log('clearAll');
+//             }
+//             if (action === 'calculate') {
+//                 const firstValue = calculator.dataset.firstValue
+//                 const operator = calculator.dataset.operator
+//                 const secondValue = displayedSelected;
+//                 a = firstValue;
+//                 b = secondValue;
+//                 total = operate(a, operator, b)
+//                 upperScreenDisplay.textContent = total;
+//             }
+
+//         }
+//     });
+// }
+
+
+// function roundLongDecimals(total) {
+//     if (total.toString().indexOf('.') !== -1) {
+//         if (total.toString().split('.')[1].length > 4) {
+//             return total.toFixed(4);
+//         }
+//     }
+//     return total;
+// }
